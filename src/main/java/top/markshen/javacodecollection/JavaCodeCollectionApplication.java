2cecae2334ac134bc31add5303a3210af22beaf9
shenjunyu <shenjunyu1030@hotmail.com>
2024-10-22 05:42:56 +0800

    feat: 初始化项目

---
+++ src/main/java/top/markshen/javacodecollection/JavaCodeCollectionApplication.java
@@ -0,0 +1,13 @@
+package top.markshen.javacodecollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCodeCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCodeCollectionApplication.class, args);
+    }
+
+}
